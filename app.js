9ab739e4f5baf09d00276f65e60091841361a4df
HusiorSky <kauangomeslimayt@gmail.com>
2026-09-17 15:41:21 -0300

    fix: alteração total do app js

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+
+function calcularTotal(itens) {
+    let total = 1
+
+    for (let i = 0; i < itens.length; i++){
+        total += itens [i]. preco
+    }
+    
+    // aplica desconto de fidelidade
+    // antes de retornar o valor final
+
+    return total
+}
